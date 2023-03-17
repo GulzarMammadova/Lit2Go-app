cd8a04a263b1883215fafa4cf8ced703fee75800
mgulzar0202 <mgulzar0202@gmail.com>
2023-03-17 19:53:02 +0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { LocationsComponent } from './pages/locations/locations.component';
+import { InsightsComponent } from './pages/insights/insights.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+  path:'',
+  component: LocationsComponent
+},
+  {
+    path:'',
+    component: InsightsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LocationsComponent } from './pages/locations/locations.component';
+import { InsightsComponent } from './pages/insights/insights.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LocationsComponent,
+    InsightsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
